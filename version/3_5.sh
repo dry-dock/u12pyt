d8e9a15a0d87b3be08eaa838b1ff4492d51c0dbd
balavignesh-s <balavignesh@shippable.com>
2015-12-14 16:24:52 +0530

    changed the vitualenv path

--- version/3_5.sh
+++ version/3_5.sh
@@ -4,8 +4,8 @@ sudo apt-get install python3.5
 wget https://bootstrap.pypa.io/get-pip.py
 python3.5 get-pip.py
 pip install virtualenv
-virtualenv -p python3.5 $HOME/venv/python3.5
+virtualenv -p python3.5 $HOME/venv/3.5
 # Install pip packages
-. $HOME/venv/python3.5/bin/activate
+. $HOME/venv/3.5/bin/activate
 pip install nose mock pytest coverage
 deactivate
