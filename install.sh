fa276a621fe98173b2341f040a65f3373821f147
himanshu0503 <hemanshu0503@gmail.com>
2016-01-25 16:37:49 +0530

    Updates files to use global pip and virtualenv

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
 
-#!/bin/bash
+# Installing pip-7.1.2
+cd /u12pyt
+python get-pip.py
+# Installing virtualenv-13.1.2
+cd /u12pyt/virtualenv-13.1.2
+python setup.py install
+cd /
+
 for file in /u12pyt/version/*;
 do
   $file
--- version/3_2.sh
+++ version/3_2.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
+echo "================= Installing Python 3.2 ==================="
 apt-get install -y python3.2 python3.2-dev
-cd /u12pyt/python3.2
-python3.2 get-pip.py
 
 # Installing virtualenv
-cd /u12pyt/python3.2/virtualenv-13.1.2
-python setup.py install
 virtualenv -p python3.2 $HOME/venv/3.2
 
 # Install pip packages
 . $HOME/venv/3.2/bin/activate
 pip install nose mock pytest coverage
 deactivate
-cd /
+echo "================= Successfully Installed Python 3.2 ==================="
