2bd4807b276b60cd9fd4aacce1658847b51e201c
himanshu0503 <hemanshu0503@gmail.com>
2016-01-25 17:16:36 +0530

    Downloads virtualenv from wget

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ apt-get update
 apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
 
 # Installing pip-7.1.2
-cd /u12pyt
+wget https://bootstrap.pypa.io/3.2/get-pip.py
 python get-pip.py
+
 # Installing virtualenv-13.1.2
-cd /u12pyt/virtualenv-13.1.2
+wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.2.tar.gz#md5=b989598f068d64b32dead530eb25589a
+tar xvzf virtualenv-13.1.2.tar.gz
+cd virtualenv-13.1.2
 python setup.py install
 cd /
 
