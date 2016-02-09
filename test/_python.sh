18ccca9ea0f3e39918657bdef3ac0f00738c90f2
ankul-shippable <ankul@shippable.com>
2016-02-09 16:09:48 +0530

    #21-check python versions

---
+++ test/_python.sh
@@ -0,0 +1,243 @@
+echo "======================= virtualenv version =========================="
+echo "virtualenv --version"
+virtualenv --version
+printf "\n\n"
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python2.6" "venv"
+virtualenv -p /usr/bin/python2.6 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python2.7" "venv"
+virtualenv -p /usr/bin/python2.7 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python3.2" "venv"
+virtualenv -p /usr/bin/python3.2 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python3.3" "venv"
+virtualenv -p /usr/bin/python3.3 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python3.4" "venv"
+virtualenv -p /usr/bin/python3.4 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/python3.5" "venv"
+virtualenv -p /usr/bin/python3.5 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/pypy" "venv"
+virtualenv -p /usr/bin/pypy venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
+
+
+echo "================ Setting up virtual environment ====================="
+echo "mkdir ~/python_test"
+mkdir ~/python_test
+echo "cd ~/python_test"
+cd ~/python_test
+echo "virtual venv"
+virtualenv venv
+echo "virtualenv -p /usr/bin/pypy3" "venv"
+virtualenv -p /usr/bin/pypy3 venv
+echo "source venv/bin/activate"
+source venv/bin/activate
+printf "\n\n"
+
+echo "======================== Python version ============================"
+echo "python --version"
+python --version
+printf "\n\n"
+
+echo "=================== Deactivating virtualenv ========================"
+echo "deactivate"
+deactivate
+echo "cd ~"
+cd ~
+echo "rm -rf python_test"
+rm -rf python_test
+echo "rm -rf venv"
+rm -rf venv
+printf "\n\n"
